1c2d669767b2ff68c09d5e1ced9cb805cf26acad
Tiago Macedo <mcdcsr.tiago@gmail.com>
2022-10-14 21:33:35 -0300

    correção para não alterar o valor inserido

--- src/SequenciaNumerica.java
+++ src/SequenciaNumerica.java
@@ -4,10 +4,12 @@ public class SequenciaNumerica {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     System.out.println("Informe o valor inicial:");
-    int inicial = sc.nextInt();
+    int valInicial = sc.nextInt();
 
     System.out.println("Informe o valor final:");
     int valFinal = sc.nextInt();
+
+    int inicial = valInicial;
 
     System.out.println("Resultado:");
     while (inicial <= valFinal){
